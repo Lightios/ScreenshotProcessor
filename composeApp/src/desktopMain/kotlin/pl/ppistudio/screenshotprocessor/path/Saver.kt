c7a6b4f81ad810950fb491ecab59cc47182c6f87
Michał Cyran <michalcyran77777@gmail.com>
2024-11-30 17:25:54 +0100

    image quality fix

--- composeApp/src/desktopMain/kotlin/pl/ppistudio/screenshotprocessor/path/Saver.kt
+++ composeApp/src/desktopMain/kotlin/pl/ppistudio/screenshotprocessor/path/Saver.kt
@@ -1,18 +1,21 @@
 package pl.ppistudio.screenshotprocessor.path
 
 import java.awt.Graphics2D
+import java.awt.RenderingHints
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
 fun resizeBufferedImage(inputImage: BufferedImage, newWidth: Int, newHeight: Int): BufferedImage {
-    val resizedImage = BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB)
+    val resizedImage = BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB)
     val graphics: Graphics2D = resizedImage.createGraphics()
+    graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC)
+    graphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+    graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     graphics.drawImage(inputImage, 0, 0, newWidth, newHeight, null)
     graphics.dispose()
     return resizedImage
 }
-
 
 fun saveImageToFile(image: BufferedImage, fileName: String): String? {
     val outputFile = File(fileName)
@@ -20,6 +23,6 @@ fun saveImageToFile(image: BufferedImage, fileName: String): String? {
     if (outputFile.exists())
         return "File already exists"
 
-    ImageIO.write(image, "png", outputFile)
+    ImageIO.write(image, "jpg", outputFile)
     return null
 }
